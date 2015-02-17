e3bd8e7f9812213c7851f7a01b9cd68e2f867912
emily.josephs <emily.josephs@capsicum.eeb.utoronto.ca>
2015-02-17 16:27:48 -0500

    removing dependencies from aseValuesByGene.py

--- aseValuesByGene.py
+++ aseValuesByGene.py
@@ -8,11 +8,9 @@ if len(sys.argv) != 6:
 	print('python aseValuesByGene.py [DNA vcf] [RNA vcf] [out file] [min snps per gene] [list of median expression levels]')
 	sys.exit()
 
-nameDict= makeDict('/cap1/emily.josephs/eQTL/bin/ind_lists/ind_list_5-13-2014_nofield')
 dnaDict = {} #keys are (scaffold,pos) and value is a list of all the hets.
 aseDict = {} #keys are (gene) value is dict where keys = ind, vals = list of diffs in het snps
-med = {} #keys are ebjInd, values are the median.
-indList = nameDict.values()
+med = {} #keys are id, values are the median.
 #make med
 medList = open(sys.argv[5],'r')
 for line in medList:
@@ -21,6 +19,7 @@ for line in medList:
 
 #read in DNA genotypes and add to dnaDict
 vcf_reader = vcf.Reader( open(sys.argv[1],'r') )
+indList = vcf_reader.samples
 for record in vcf_reader:
         hets = [x.sample for x in record.get_hets()]
         dnaDict[(record.CHROM,record.POS)] = hets
@@ -38,23 +37,22 @@ for record in rna_reader:
 		#is this ind in our 99 samples?
 		if ind not in nameDict.keys():
 			continue
-		ebjInd = nameDict[ind] #switch YW id to EBJ id
 		
-		if ebjInd not in aseDict[record.CHROM].keys():
-			aseDict[record.CHROM][ebjInd] = [] #add gene to aseDict keys if not there already
+		if ind not in aseDict[record.CHROM].keys():
+			aseDict[record.CHROM][ind] = [] #add gene to aseDict keys if not there already
 		try:
-			ad = record.genotype(ebjInd).data.AD
+			ad = record.genotype(ind).data.AD
 		except:
-			print(ebjInd)
+			print(ind)
 			print(record)
-			print(record.samples)
+			print(record.samples
 			print('')  #want to make sure I'm not missing cases of complete ASE
 			continue
 		if ad == None: #missing data
 			continue
 
-		aseDif = float(abs(ad[1] - ad[0]))/med[ebjInd]
-		aseDict[record.CHROM][ebjInd].append(aseDif)
+		aseDif = float(abs(ad[1] - ad[0]))/med[ind]
+		aseDict[record.CHROM][ind].append(aseDif)
 
 # print out --  table where rows are genes, columns are genotypes
 out = open(sys.argv[3],'a')
